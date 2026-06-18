224e913420be5962e2f6556d9f08333055091f08
netPokey <netcsk@qq.com>
2026-06-18 12:52:43 -0400

    refactor(analyzer): minimize assembly point to listen+web link

--- src/can_analyzer.cpp
+++ src/can_analyzer.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <LittleFS.h>
 #include <esp_heap_caps.h>
-#include <cstring>
 #include <memory>
 
 #include "analyzer/analyzer_control.h"
@@ -10,16 +9,7 @@
 #include "analyzer/bus_stats.h"
 #include "analyzer/frame_queue.h"
 #include "analyzer/id_table.h"
-#include "analyzer/common_signal_store.h"
-#include "analyzer/label_store.h"
-#include "analyzer/pretrigger_buffer.h"
-#include "analyzer/recorder.h"
-#include "analyzer/replay_service.h"
 #include "analyzer/rx_task.h"
-#include "analyzer/signal_window.h"
-#include "analyzer/snapshot_store.h"
-#include "analyzer/tx_service.h"
-#include "analyzer/tx_mode_sync.h"
 #include "can_helpers.h"
 #include "drivers/mcp2515_driver.h"
 #include "drivers/twai_driver.h"
@@ -34,36 +24,8 @@ FrameQueue g_queue;
 IdTable g_table;
 BusStatsTracker g_stats;
 
-constexpr size_t kPretriggerCapacity = 16384;
-CapturedFrame *g_pretriggerStorage = nullptr;
-PretriggerBuffer g_pretrigger;
-SnapshotRecord *g_snapshotA = nullptr;
-SnapshotRecord *g_snapshotB = nullptr;
-SnapshotStore g_snapshots;
-LabelStore g_labels;
-
-constexpr size_t kSignalWatchSlots = 4;
-constexpr size_t kSignalSamplesPerSlot = 64;
-WindowSlot *g_signalSlots = nullptr;
-RawSamplePoint *g_signalSamples = nullptr;
-WatchedSignalWindow g_signalWindow;
-CommonSignalStore g_commonSignals;
-
-constexpr size_t kRecordCapacity = 100000;   // ~24B/帧 ≈ 2.4MB PSRAM
-CapturedFrame *g_recordStorage = nullptr;
-Recorder g_recorder;
-CapturedFrame *g_replayStorage = nullptr;
-ReplayService g_replayService;
-
 std::unique_ptr<MCP2515Driver> g_canA;
 std::unique_ptr<TWAIDriver> g_canB;
-TxService g_txService;
-TxModeSync g_txModeSync;
-
-void syncTxMode()
-{
-    g_txModeSync.sync(g_canA.get(), g_canB.get());
-}
 }
 
 void setup()
@@ -71,8 +33,6 @@ void setup()
     Serial.begin(115200);
     delay(1000);
     setCanTxEnabled(false);
-    setAnalyzerChannelTxEnabled(0, false);
-    setAnalyzerChannelTxEnabled(1, false);
 
     if (!LittleFS.begin(true))
         Serial.println("LittleFS mount failed");
@@ -89,61 +49,11 @@ void setup()
     g_queue.init(g_queueStorage, kQueueCapacity);
     g_stats.begin(millis());
 
-    g_pretriggerStorage = static_cast<CapturedFrame *>(ps_malloc(sizeof(CapturedFrame) * kPretriggerCapacity));
-    if (g_pretriggerStorage)
-        g_pretrigger.init(g_pretriggerStorage, kPretriggerCapacity);
-    else
-        Serial.println("PSRAM allocation failed for pretrigger buffer");
-
-    const size_t snapshotBytes = sizeof(SnapshotRecord) * kChannelCount * kStdIdCount;
-    g_snapshotA = static_cast<SnapshotRecord *>(ps_malloc(snapshotBytes));
-    g_snapshotB = static_cast<SnapshotRecord *>(ps_malloc(snapshotBytes));
-    if (g_snapshotA && g_snapshotB)
-        g_snapshots.init(g_snapshotA, g_snapshotB);
-    else
-        Serial.println("PSRAM allocation failed for snapshot buffers");
-
-    g_labels.begin();
-
-    g_signalSlots = static_cast<WindowSlot *>(ps_malloc(sizeof(WindowSlot) * kSignalWatchSlots));
-    g_signalSamples = static_cast<RawSamplePoint *>(ps_malloc(sizeof(RawSamplePoint) * kSignalWatchSlots * kSignalSamplesPerSlot));
-    if (g_signalSlots && g_signalSamples)
-    {
-        memset(g_signalSlots, 0, sizeof(WindowSlot) * kSignalWatchSlots);
-        memset(g_signalSamples, 0, sizeof(RawSamplePoint) * kSignalWatchSlots * kSignalSamplesPerSlot);
-        for (size_t i = 0; i < kSignalWatchSlots; ++i)
-            g_signalSlots[i].samples = g_signalSamples + (i * kSignalSamplesPerSlot);
-        g_signalWindow.init(g_signalSlots, kSignalWatchSlots, kSignalSamplesPerSlot);
-    }
-    else
-    {
-        Serial.println("PSRAM allocation failed for signal watch window");
-        g_signalSlots = nullptr;
-        g_signalSamples = nullptr;
-    }
-
-    g_commonSignals.begin();
-
-    g_recordStorage = static_cast<CapturedFrame *>(ps_malloc(sizeof(CapturedFrame) * kRecordCapacity));
-    if (g_recordStorage)
-        g_recorder.init(g_recordStorage, kRecordCapacity);
-    else
-        Serial.println("PSRAM allocation failed for recorder");
-
-    g_replayStorage = static_cast<CapturedFrame *>(ps_malloc(sizeof(CapturedFrame) * kRecordCapacity));
-    if (!g_replayStorage)
-        Serial.println("PSRAM allocation failed for replay buffer");
-
     g_canA.reset(new MCP2515Driver(MCP2515_CS, MCP2515_RST,
                                    MCP2515_SCLK, MCP2515_MISO, MCP2515_MOSI,
                                    &SPI, 10000000));
     g_canB.reset(new TWAIDriver(static_cast<gpio_num_t>(CAN_TX),
                                 static_cast<gpio_num_t>(CAN_RX)));
-    g_txService.init(g_canA.get(), g_canB.get());
-    g_replayService.init(g_recordStorage ? &g_recorder : nullptr,
-                         &g_txService,
-                         g_replayStorage,
-                         g_replayStorage ? kRecordCapacity : 0);
 
     const bool canAOk = g_canA->init();
     const bool canBOk = g_canB->init();
@@ -155,7 +65,6 @@ void setup()
     if (!canBOk)
         Serial.println("CAN_B init failed");
 
-    syncTxMode();
     if (canAOk)
         g_canA->setFilters(nullptr, 0);
     if (canBOk)
@@ -166,15 +75,7 @@ void setup()
                 &g_queue);
 
     const String ip = analyzerWifiBegin();
-    analyzerWebSetContext(&g_queue, &g_table, &g_stats,
-                          g_pretriggerStorage ? &g_pretrigger : nullptr,
-                          (g_snapshotA && g_snapshotB) ? &g_snapshots : nullptr,
-                          &g_labels,
-                          (g_signalSlots && g_signalSamples) ? &g_signalWindow : nullptr,
-                          &g_commonSignals,
-                          g_recordStorage ? &g_recorder : nullptr,
-                          &g_txService,
-                          &g_replayService);
+    analyzerWebSetContext(&g_queue, &g_table, &g_stats);
     analyzerWebBegin();
 
     Serial.print("CAN analyzer ready (listen-only): http://");
@@ -183,7 +84,6 @@ void setup()
 
 void loop()
 {
-    syncTxMode();
     analyzerWebLoop();
     delay(1);
 }
